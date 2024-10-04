58caa1e1f54e63b7ac189c0f717f1ac22a849bfc
Murphy McCauley <murphy.mccauley@gmail.com>
2024-10-04 05:36:36 -0400

    recoco: Fix indenting in examples module

--- pox/lib/recoco/examples.py
+++ pox/lib/recoco/examples.py
@@ -24,60 +24,53 @@ from pox.lib.revent import *
 from pox.lib.recoco import *
 
 class EventLoopExample (Task):
-   """
-   Suppose we have a component of our application that uses it's own event
-   loop. recoco allows us to "add" our select loop to the other event
-   loops running within pox.
+  """
+  Suppose we have a component of our application that uses it's own event
+  loop. recoco allows us to "add" our select loop to the other event
+  loops running within pox.
 
-   First note that we inherit from Task. The Task class is recoco's equivalent
-   of python's threading.thread interface.
-   """
-   def __init__(self):
-     Task.__init__(self)  # call our superconstructor
+  First note that we inherit from Task. The Task class is recoco's equivalent
+  of python's threading.thread interface.
+  """
+  def __init__ (self):
+    Task.__init__(self)  # call our superconstructor
 
-     self.sockets = self.get_sockets() # ... the sockets to listen to events on
+    self.sockets = self.get_sockets() # ... the sockets to listen to events on
 
-     # Note! We can't start our event loop until the core is up. Therefore,
-     # we'll add an event handler.
-     core.addListener(pox.core.GoingUpEvent, self.start_event_loop)
+    # Note! We can't start our event loop until the core is up. Therefore,
+    # we'll add an event handler.
+    core.addListener(pox.core.GoingUpEvent, self.start_event_loop)
 
-   def start_event_loop(self, event):
-     """
-     Takes a second parameter: the GoingUpEvent object (which we ignore)
-     """
-     # This causes us to be added to the scheduler's recurring Task queue
-     Task.start(self)
+  def start_event_loop (self, event):
+    """
+    Takes a second parameter: the GoingUpEvent object (which we ignore)
+    """
+    # This causes us to be added to the scheduler's recurring Task queue
+    Task.start(self)
 
-   def get_sockets(self):
-     return []
+  def get_sockets (self):
+    return []
 
-   def handle_read_events(self):
-      pass
+  def handle_read_events (self):
+    pass
 
-   def run(self):
-     """
-     run() is the method that gets called by the scheduler to execute this task
+  def run (self):
+    """
+    run() is the method that gets called by the scheduler to execute this task
+    """
+    while core.running:
       """
-     while core.running:
-       """
-       This looks almost exactly like python's select.select, except that it's
-       it's handled cooperatively by recoco
+      This looks almost exactly like python's select.select, except that it's
+      it's handled cooperatively by recoco
 
-       The only difference in Syntax is the "yield" statement, and the
-       capital S on "Select"
-       """
-       rlist,wlist,elist = yield Select(self.sockets, [], [], 3)
-       events = []
-       for read_sock in rlist:
-         if read_sock in self.sockets:
-           events.append(read_sock)
+      The only difference in Syntax is the "yield" statement, and the
+      capital S on "Select"
+      """
+      rlist,wlist,elist = yield Select(self.sockets, [], [], 3)
+      events = []
+      for read_sock in rlist:
+        if read_sock in self.sockets:
+          events.append(read_sock)
 
-         if events:
-           self.handle_read_events() # ...
-
-
-"""
-And that's it!
-
-TODO: write example usages of the other recoco BlockingTasks, e.g. recoco.Sleep
-"""
+        if events:
+          self.handle_read_events() # ...
